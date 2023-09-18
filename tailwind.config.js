d6457064ae2dcca188e8a4cfe6edc8d237a8cef8
unknown <thachnguyen281995@gmail.com>
2023-09-18 17:47:29 +0700

    upload source

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  purge: ["./**/*.html", "./**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    // ...
+  },
+  // ...
+};
